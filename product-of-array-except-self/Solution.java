d24ea364036aa37741552019a1eca3293fa08ab5
henqeb <henrikbellsund@outlook.com>
2024-01-30 16:05:34 +0100

    Add more optimal soluten using linear time by calculating subarrays with pre- and suffix products

--- product-of-array-except-self/Solution.java
+++ product-of-array-except-self/Solution.java
@@ -1,15 +1,35 @@
 public class Solution {
 
-    public static int[] productExceptSelf(int[] nums) {
-        int[] products = new int[nums.length];
+    public static void main(String[] args) {
+        productExceptSelf(new int[]{1, 2, 3, 4});
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            int product = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (j == i) continue;
-                product *= nums[j];
-            }
-            products[i] = product;
+    /*
+     * 
+     */
+    public static int[] productExceptSelf(int[] nums) {
+        final int N = nums.length;
+        int[] products = new int[N];
+        
+        // products from left to right
+        int[] prefixProducts = new int[N];
+        int prod = 1;
+        for (int i = 0; i < N; i++) {
+            prefixProducts[i] = prod;
+            prod *= nums[i];
+        }
+
+        // products from right to left
+        int[] suffixProducts = new int[N];
+        prod = 1;
+        for (int i = N - 1; i >= 0; i--) {
+            suffixProducts[i] = prod;
+            prod *= nums[i];
+        }
+
+        // multiplying across pre-/suffix products to get result
+        for (int i = 0; i < N; i++) {
+            products[i] = prefixProducts[i] * suffixProducts[i];
         }
 
         return products;
