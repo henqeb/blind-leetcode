883ecd4bbc01e2ca1cd7ad0afa7775a170419499
henqeb <henrikbellsund@outlook.com>
2024-01-30 13:54:57 +0100

    Add suboptimal solution using O(n^2) time

---
+++ product-of-array-except-self/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] products = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) continue;
+                product *= nums[j];
+            }
+            products[i] = product;
+        }
+
+        return products;
+    }
+}
